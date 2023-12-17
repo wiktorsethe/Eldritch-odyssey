9ef73822f36658cf1f1facb3edcd56c4f20d032b
Wiktor Szczepanik <wiktor_szczepanik@wp.pl>
2023-12-17 23:22:23 +0100

    player movement i zapoznanie z tilemap

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        rb.velocity = direction * speed;
+
+    }
+
+    /*
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        // Get input values
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate movement vector
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f);
+        movement.Normalize(); // Normalize to ensure consistent speed in all directions
+
+        // Move the player
+        transform.position += movement * moveSpeed * Time.deltaTime;
+    }
+    */
+}
