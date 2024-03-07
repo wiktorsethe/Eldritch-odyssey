28af7e5f64532e2c88b23025198206f20a3dd620
Wiktor Szczepanik <wiktor_szczepanik@wp.pl>
2024-03-07 15:07:24 +0100

    Mirror plugin

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Netcode;
-
+using Mirror;
 public class PlayerMovement : NetworkBehaviour
 {
     [SerializeField] private float speed;
@@ -17,6 +16,8 @@ public class PlayerMovement : NetworkBehaviour
 
     private void FixedUpdate()
     {
+        if (!isLocalPlayer) return;
+
         direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
         rb.velocity = direction * speed;
 
